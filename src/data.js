1806cc2372c1bf949803e0389e86cf41e21f44df
นาย ปัญจพล อ่อนโคทา <58883256+Punjapon@users.noreply.github.com>
2021-11-22 04:19:02 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,48 +2,42 @@ const data = {
   products: [
     {
       id: '1',
-      name: 'GTR R35',
-      price: 15500000
-      image: 'https://storage.googleapis.com/autostation-com/2021/09/d11e57d3-nissan-gt-r-t-spec-13.jpg',
-    },
-    {
-      id: '2',
       name: 'GTR R34',
       price: 12000000,
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnoQQ8JP0HcdDUVda56YmoqzlcqMtdfV0dUN0L5qN9kb1O6_d7DWOqbfqCYEZIXPLF2Yw&usqp=CAU',
     },
     {
-      id: '3',
+      id: '2',
       name: 'GTR R33',
       price: 10000000,
       image: 'https://xo-autosport.grandprix.co.th/wp-content/uploads/2019/07/Story-of-R33.jpg',
     },
     {
-      id: '4',
+      id: '3',
       name: 'GTR R32',
       price: 11000000,
       image: 'https://www.checkraka.com/uploaded/article/news/1367039/Nissan-Skyline-GT-R-(R32)-1.jpg',
-    }
+    },
     {
-      id: '5',
+      id: '4',
       name: 'Rx7',
       price: 13000000,
       image: 'http://www.headlightmag.com/hlmwp/wp-content/uploads/2020/05/rx-72.jpg',
-    }
+    },
     {
-      id: '6',
+      id: '5',
       name: 'BRZ',
       price: 15000000,
       image: 'https://motortrivia.com/wp-content/uploads/2021/04/subaru-brz-2nd-gen-2022my-01.jpg',
-    }
+    },
     {
-      id: '7',
+      id: '6',
       name: 'AE86',
       price: 60000000,
       image: 'https://img.gta5-mods.com/q95/images/1985-toyota-sprinter-trueno-gt-apex-ae86-add-on/466f36-1.jpg',
-    }
+    },
     {
-      id: '8',
+      id: '7',
       name: 'Supra',
       price: 14000000,
       image: 'https://www.grandprix.co.th/wp-content/uploads/2020/01/supra-1.jpg',
